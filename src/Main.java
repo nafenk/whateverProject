72d6bf10b73c1279c4d243a44bf5faf2f2da358c
Nafen <gushajana@student.gu.se>
2018-12-23 18:08:00 +0100

    fun stuff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[]arg){
         System.out.println("finally i learned how to use github!! ");
+        Scanner mspacman = new Scanner(System.in);
+
+        System.out.println("hey whatsup!! ");
+        String greetings = mspacman.nextLine();
+        if(!greetings.equals("hi")){
+            System.out.println("dude don't be rude");
+        }
+
     }
 }
